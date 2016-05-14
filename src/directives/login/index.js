5e13af5beb212163a3bed65e3fe4a156bc5899be
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-05-14 12:39:49 +0200

    login page + redirect

--- src/directives/login/index.js
+++ src/directives/login/index.js
@@ -4,15 +4,33 @@ require('./login.scss');
 const template = `
 <h1>Login</h1>
 <button class="button" ng-click="login()" ng-if="!$root.authData">Login</button>
+<button class="button" ng-click="logout()" ng-if="$root.authData">Logout</button>
 `;
 
-function LoginPage() {
-    return {
-        template
+function controller($scope, Auth) {
+
+    $scope.login = function () {
+        if ($scope.authData) {
+            console.info('Already Logged in');
+        } else {
+            Auth.$authWithOAuthPopup("facebook").then(function (authData) {
+                console.log("Sucess Login", authData);
+            }).catch(function (error) {
+                console.log("Login failed :", error);
+            });
+        }
     };
+
+    $scope.logout = function () {
+        Auth.$unauth();
+    };
+
 }
 
 const LoginPageModule = angular.module('app.directives.results.loginpage', [])
-    .directive('loginPage', LoginPage);
+    .component('loginPage', {
+        template,
+        controller
+    });
 
 export default LoginPageModule.name;
